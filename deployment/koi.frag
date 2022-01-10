871625ffefa81d60c6cc76e775f0c6ab23f5daae
Ricky Chu <ricky@rchu.cc>
2022-01-10 15:59:40 +0000

    started koi

---
+++ deployment/koi.frag
@@ -0,0 +1,36 @@
+
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform int u_frameCount;
+uniform vec2 u_trail_positions[30];
+uniform vec2 u_anchor_positions[6];
+
+void main() {
+    vec2 uv = gl_FragCoord.xy/u_resolution.xy;
+    uv -= 0.5;
+    uv.x *= u_resolution.x/u_resolution.y;
+
+    float r = 0.;
+    float g = 0.;
+    float b = 0.;
+
+    float start, idx, d, val;
+    for (int i = 0; i < 30; i++){
+        d = length(uv - u_trail_positions[i]);
+        val = (float(i)/20.) / d * 0.001;
+        r += 0.5*val;
+        g += 0.5*val;
+    }
+  
+    for (int i = 0; i < 6; i++){
+      d = length(uv - u_anchor_positions[i]);
+      val = smoothstep(0.01, 0.0011, d);
+      b += val;
+    }
+    gl_FragColor = vec4(r, g, b, 1.0);
+}
+
+
---
+++ deployment/koi.js
@@ -0,0 +1,169 @@
+//Ported from racket
+
+class Player {
+    constructor(x,y, xVel, yVel){
+        this.pos = createVector(x,y);
+        this.vel = createVector(xVel,yVel);
+        
+        this.attached = false;
+        this.direction = 1;
+        this.trailLen = 30;
+        this.trail = this.initTrail();
+    }
+  
+    initTrail(){
+      let t = [];
+      for (let i = 0; i < this.trailLen; i++){
+        t.push(createVector(0,0));
+      }
+      return t;
+    }
+    
+    toData(){
+        let data = [];
+        //https://soegaard.github.io/sketching/Examples.html
+        for (let i = 0; i < this.trailLen; i++){
+            let start = frameCount % this.trailLen;
+            let idx = (start + 1 + i) % this.trailLen;
+
+            let x = this.trail[idx].x;            
+            let y = this.trail[idx].y; 
+            x = map(x, 0, width, -0.5, 0.5);
+            y = map(y, 0, height, -0.5, 0.5);
+            data.push(x,y);
+        }
+        return data;
+    }
+
+    update(){
+        this.pos.add(this.vel);
+        this.vel.mult(0.999);
+        
+        if ((this.pos.x > width) || (this.pos.x < 0)){
+            this.vel.x *= -1; 
+        }
+
+        if ((this.pos.y > height) || (this.pos.y < 0)){
+           this.vel.y *= -1; 
+        }
+
+        if (this.attached){
+            let neoVel = this.perp(10);
+            this.vel.x = this.direction * neoVel.x;
+            this.vel.y = this.direction * neoVel.y;
+
+            if (this.vel.mag() > 60){
+                this.vel = this.vel.normalize().mult(40); 
+            }
+        }
+
+        this.trail[frameCount % this.trailLen] = this.pos;
+    }
+
+    getDirection(){
+        let diff = p5.Vector.sub(this.pos, this.attached);
+        let det = (diff.x - this.vel.y) * (diff.y - this.vel.x);
+
+        return (det > 0) ? -1 : 1; 
+    }
+
+    attach(pointList){
+        this.attached = this.nearest(pointList);
+        this.direction = this.getDirection();
+    }
+
+    detatch(){
+        this.attached = false;
+        if (this.pos.x > width){
+            this.pos.x = 10;
+        }
+        else if (this.pos.x < 0){
+            this.pos.x = width - 10;
+        }
+        if (this.pos.y > height){
+            this.pos.y = 10;
+        }
+        else if (this.pos.y < 0){
+            this.pos.y = height - 10;
+        }
+    }
+
+    perp(multiplier){
+        let diff = p5.Vector.sub(this.pos, this.attached);
+        let n = diff.normalize();
+        n.mult(multiplier);
+
+        return createVector(-n.y, n.x);
+    }
+    
+    nearest(pointList){
+        let minD = Infinity;
+        let point;
+        for (let i = 0; i < pointList.length; i++){
+            let d = this.pos.dist(pointList[i]);
+
+            if (d < minD){
+                minD = d;
+                point = pointList[i];
+            }
+        }
+        return point;
+    }
+
+    render(){
+        stroke(255);
+        circle(this.pos.x, this.pos.y, 10);
+
+        if (this.attached){
+            line(this.pos.x, this.pos.y, this.attached.x, this.attached.y);
+        }
+    }
+}
+
+var balls = [];
+var ballsData = [];
+var me;
+var theShader;
+
+ function preload() {
+     theShader = loadShader("koi.vert", "koi.frag");
+}
+
+function setup(){
+    createCanvas(windowWidth, windowHeight, WEBGL);
+    //noCursor();
+    noStroke();
+
+    me = new Player(200, 200, 5, 4);
+    for (let i = 0; i < 6; i++){
+        balls[i] = createVector(random()* width,
+                                random()* height);
+        let x = map(balls[i].x, 0, width, -0.5, 0.5);
+        let y = map(balls[i].y, 0, height, -0.5, 0.5);
+        ballsData.push(x,y);
+
+    }
+}
+
+function windowResized(){
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    me.update();
+    shader(theShader);
+    theShader.setUniform("u_resolution", [width, height]);
+    theShader.setUniform("u_frameCount", frameCount)
+    theShader.setUniform("u_trail_positions", me.toData());
+    theShader.setUniform("u_anchor_positions", ballsData);
+    rect(0,0, width, height);
+}
+
+function keyPressed(){
+    console.log("hi");
+    me.attach(balls);
+}
+
+function keyReleased(){
+    me.detatch();
+}
